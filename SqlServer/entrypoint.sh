8387a0c26298912df7ac9c6866c59b01407a73f3
Ravichandran Rathinam <znrrm1@wowcorp.com.au>
2019-12-06 23:56:19 +1100

    latest changes

--- SqlServer/entrypoint.sh
+++ SqlServer/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Jko3va-D9821jhsvGD -d master -i /var/opt/mssql/data/CreateCustomerDB.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Jko3va-D9821jhsvGD -d master -i /var/opt/mssql/data/setup.sql
 
 exec "$@"
